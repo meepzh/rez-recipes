8b23db945f9abb1ade3981d3814c9a661e300dae
Robert Zhou <meep.zh@gmail.com>
2023-12-26 13:17:36 -0800

    Add PySide2

---
+++ PySide2/_native/package.py
@@ -0,0 +1,64 @@
+name = "PySide2"
+
+
+authors = ["Robert Zhou"]
+
+
+build_command = False
+
+
+def commands():
+    env.QT_PREFERRED_BINDING = "PySide2"
+
+    if building and this._cmake_path:
+        env.CMAKE_MODULE_PATH.append("{this._cmake_path}")
+
+
+description = (
+    "The official Python module from the Qt for Python project, which provides access "
+    "to the complete Qt 5.12+ framework."
+)
+
+
+@early()
+def requires():
+    from rez.package_py_utils import find_site_python
+
+    python_pkg = find_site_python("PySide2")
+    return [python_pkg.qualified_name]
+
+
+uuid = "recipes.PySide2.native"
+
+
+variants = [["platform-**", "arch-**", "os-**"]]
+
+
+@early()
+def version():
+    return this.__version + "-native"
+
+
+@early()
+def _cmake_path():
+    import pathlib
+    from rez.package_py_utils import exec_command
+
+    try:
+        out, err = exec_command("tools", ["pacman", "-Ql", "pyside2"])
+    except FileNotFoundError:
+        return None
+    else:
+        for path in out.split("\n"):
+            if path.endswith(".cmake"):
+                path = path.partition(" ")[2]
+                return str(pathlib.Path(path).parent)
+
+
+def _version():
+    from rez.package_py_utils import exec_python
+
+    return exec_python("version", ["import PySide2", "print(PySide2.__version__)"])
+
+
+__version = _version()
