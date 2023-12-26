55c20776d22f4ce7be85709fc791288f2c903a1d
Robert Zhou <meep.zh@gmail.com>
2023-12-26 12:38:09 -0800

    First commit with Python native

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rez-recipes
+<a href="https://opensource.org/license/mit/"><img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-green.svg"></a>
+<a href="https://github.com/psf/black"><img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"></a>
+
 ASWF Rez package recipes primarily targeting Arch Linux
---
+++ python/_native/package.py
@@ -0,0 +1,112 @@
+name = "python"
+
+
+authors = ["Robert Zhou"]
+
+
+build_command = False
+
+
+def commands():
+    env.PATH.append("{this._bin_path}")
+
+    if building and this._cmake_path:
+        env.CMAKE_MODULE_PATH.append("{this._cmake_path}")
+
+
+description = (
+    "An interpreted, object-oriented, high-level programming language with dynamic "
+    "semantics."
+)
+
+
+@early()
+def tools():
+    from rez.package_py_utils import exec_command
+
+    tools = []
+
+    try:
+        out, err = exec_command("tools", ["pacman", "-Ql", "python"])
+    except FileNotFoundError:
+        py_major, py_minor, _ = this.__version.split(".")
+        for version in (
+            "",
+            py_major,
+            f"{py_major}.{py_minor}",
+        ):
+            for tool in (
+                "2to3",
+                "idle",
+                "pydoc",
+                "python",
+            ):
+                tools.append(f"{tool}{version}")
+    else:
+        for path in out.split("\n"):
+            if not path.startswith("python /usr/bin"):
+                continue
+            tool = path.rpartition("/")[2]
+            if tool:
+                tools.append(tool)
+
+    return tools
+
+
+uuid = "recipes.python.native"
+
+
+variants = [["platform-**", "arch-**", "os-**"]]
+
+
+@early()
+def version():
+    return this.__version + "-native"
+
+
+@early()
+def _bin_path():
+    from rez.package_py_utils import exec_python
+
+    return exec_python(
+        "_bin_path",
+        ["import pathlib", "import sys", "print(pathlib.Path(sys.executable).parent)"],
+    )
+
+
+@early()
+def _cmake_path():
+    import pathlib
+    from rez.package_py_utils import exec_command
+
+    try:
+        out, err = exec_command("tools", ["pacman", "-Ql", "cmake"])
+    except FileNotFoundError:
+        return None
+    else:
+        for path in out.split("\n"):
+            if path.endswith("FindPython.cmake"):
+                path = path.partition(" ")[2]
+                return str(pathlib.Path(path).parent)
+
+
+@early()
+def _site_paths():
+    import ast
+    from rez.package_py_utils import exec_python
+
+    paths_literal = exec_python(
+        "_site_paths", ["import site", "print(site.getsitepackages())"]
+    )
+    return ast.literal_eval(paths_literal)
+
+
+def _version():
+    from rez.package_py_utils import exec_python
+
+    return exec_python(
+        "version", ["import platform", "print(platform.python_version())"]
+    )
+
+
+__version = _version()
