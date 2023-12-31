5d3f6d94113ebca1e7f54defddb8dd4da1bde6ca
Robert Zhou <meep.zh@gmail.com>
2023-12-31 15:40:41 -0800

    Add support for reading version information directly from zip

--- SHAPES/_local/package.py
+++ SHAPES/_local/package.py
@@ -1,6 +1,8 @@
 """To make use of this recipe, please provide a path to the SHAPES zip file using the
 SHAPES_DOWNLOAD CMake variable.
 """
+import typing
+
 import maya_packaging
 
 
@@ -40,31 +42,84 @@ def variants():
 
 @early()
 def version():
-    import os
+    from rez.exceptions import InvalidPackageError
+
+    version = _find_version_from_build()
+    if not version:
+        version = _find_version_from_args()
+    if version:
+        return version
+
+    raise InvalidPackageError("Could not find a version number to use from versions.md")
+
+
+def _extract_version_from_md(md_file: typing.IO) -> str:
+    """Searches the versions.md file for the first version string it can find.
+
+    Returns:
+        The version, if found.
+    """
+    import re
+
+    version_pattern = re.compile(r"\b\d+\.\d+\.\d+\b")
+
+    for line in md_file:
+        match = version_pattern.search(str(line))
+        if match:
+            return match.group(0)
+
+    return None
+
+
+def _find_version_from_args() -> str:
+    """Searches the arguments to rez-build for the SHAPES download to extract and search
+    for version information.
+
+    Returns:
+        The version, if found.
+    """
     import pathlib
     import re
-    from rez.exceptions import InvalidPackageError
+    import sys
+
+    download_path = None
+    variable_pattern = re.compile(r"-DSHAPES_DOWNLOAD(:\w+)?=(.*)")
+    for arg in sys.argv:
+        match = variable_pattern.search(arg)
+        if match:
+            download_path = match.group(2) or download_path
+
+    if download_path:
+        from zipfile import ZipFile
+
+        with ZipFile(download_path) as download_file:
+            with download_file.open(
+                str(pathlib.Path("SHAPES", "versions.md"))
+            ) as versions_file:
+                return _extract_version_from_md(versions_file)
+
+    return None
+
+
+def _find_version_from_build() -> str:
+    """Searches the build directory for version information on SHAPES.
+
+    Returns:
+        The version, if found.
+    """
+    import os
+    import pathlib
 
     versions_path = None
     for dirpath, dirnames, filenames in os.walk("build"):
         if "versions.md" in filenames:
             versions_path = pathlib.Path(dirpath, "versions.md")
 
-    if not versions_path:
-        raise InvalidPackageError(
-            "Could not find the build directory versions.md file required for "
-            "determining the version number"
-        )
+    if versions_path:
+        with open(versions_path) as versions_file:
+            return _extract_version_from_md(versions_file)
 
-    version_pattern = re.compile(r"\b\d+\.\d+\.\d+\b")
-
-    with open(versions_path) as versions_file:
-        for line in versions_file:
-            match = version_pattern.search(line)
-            if match:
-                return match.group(0)
-
-    raise InvalidPackageError("Could not find a version number to use from versions.md")
+    return None
 
 
 __python_version = maya_packaging.get_python_version()
